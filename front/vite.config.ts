6b9c33a58b73719a235dc8b73007c3e7fd3b8071
Fournet Enzo <enzofournet31@gmail.com>
2026-04-09 16:45:50 +0200

    feat 86c98x1v9 add tailwind css

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 const apiProxyTarget =
   process.env.VITE_DEV_PROXY_TARGET ?? 'http://backend:5000'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   server: {
     allowedHosts: ['localhost', '127.0.0.1', 'flowrank.tigropoil.fr'],
     proxy: {
