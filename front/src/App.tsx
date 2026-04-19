3b1de95daa92a365c67eb541c77d13fdfee28a23
paola.tosi <ptosi5272@gmail.com>
2026-04-19 13:39:37 +0200

    fix: remove missing auth routes for current branch build

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Accueil from "./components/Accueil/Accueil";
-import AuthCallback from "./components/AuthCallback/AuthCallback";
 import CreationCompte from "./components/CompteCreation/CreationCompte";
-import LierCompte from "./components/LierCompte/LierCompte";
 
 function App() {
   return (
@@ -10,8 +8,6 @@ function App() {
       <Route path="/" element={<Navigate to="/connexion" replace />} />
       <Route path="/connexion" element={<Accueil />} />
       <Route path="/creationCompte" element={<CreationCompte />} />
-      <Route path="/lierCompte" element={<LierCompte />} />
-      <Route path="/auth/callback" element={<AuthCallback />} />
       <Route path="*" element={<Navigate to="/connexion" replace />} />
     </Routes>
   );
